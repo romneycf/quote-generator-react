308f72e46ce0f92578ad4d9cdbc8bf901d69a135
romneycf <romneycf@gmail.com>
2023-03-24 16:19:54 -0300

    quote 'api' url

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Card from "./components/card/card";
 import Title from "./components/title/title";
 
 function App() {
+  function handleClick(){
+    console.log('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
+  }
   return (
     <div className="App">
       <Title>
@@ -13,7 +16,7 @@ function App() {
       <Card>
         <div id="quote-box">
         </div>
-        <Button>Carlos</Button>
+        <Button onClick={handleClick}>Carlos</Button>
       </Card>
     </div>
   );
