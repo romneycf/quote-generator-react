edcfe12adf35cbf961fcc05be43228d1c46b9329
romneycf <romneycf@gmail.com>
2023-03-27 15:06:02 -0300

    funcionalidade 100%

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/button/button";
 import Card from "./components/card/card";
@@ -6,18 +6,29 @@ import Title from "./components/title/title";
 import { fetchQuotes } from "./entities/quotes";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    handleClick();
+  }, []);
+
   async function randomQuote() {
     const quotes = await fetchQuotes();
-    console.log(quotes);
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex];
-    console.log(randomQuote);
-    const quotebox = document.querySelector("#text");
-    const authorbox = document.querySelector("#author");
+    setQuote('"' + randomQuote.quote + '"');
+    setAuthor("-" + randomQuote.author);
   }
 
   async function handleClick() {
-    randomQuote();
+    await randomQuote();
+  }
+
+  function handleWho(){
+    const name = author.replace(/-/g, "").trim();
+    const url = "https://www.google.com/search?q="+name 
+    window.open(url, '_blank');
   }
 
   return (
@@ -25,14 +36,17 @@ function App() {
       <Title>Random Quote Generator " -"</Title>
       <Card>
         <div id="quote-box">
-          <div id="text">'QUOTE'</div>
-          <div id="author">'AUTHOR'</div>
+          <div id="text">{quote}</div>
+          <div id="author">{author}</div>
           <div id="buttons-wrapper">
-            <a id="tweet-quote" href="">
-              twister
+            <a title="Twister" id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank">
+              <i className="fa-brands fa-twitter"></i>
             </a>
-            <Button id="new-quote" onClick={handleClick}>
-              New Quote
+            <Button title="Who?" id="who" onClick={handleWho}>
+              ?
+            </Button>
+            <Button title="New quote" id="new-quote" onClick={handleClick}>
+              <i className="fa fa-arrows-rotate"> </i>
             </Button>
           </div>
         </div>
